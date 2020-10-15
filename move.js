6b43e85714231107c0d299572bda021caf550ea8
Tempate <ddiazq28@gmail.com>
2020-10-15 19:06:29 +0200

    Allow single moves to have two coordinates

--- move.js
+++ move.js
@@ -4,11 +4,20 @@ const Util = require('./util')
 function createMove(to, from) {
     if (to == undefined) {
         type = MoveType.Null;
-    } else if (from == undefined || Util.distance(from, to) == 1) {
-        type = MoveType.Single
-        from = -1
+    } else if (from == undefined) {
+        type = MoveType.Single;
+        from = -1;
     } else {
-        type = MoveType.Double
+        switch (Util.distance(from, to)) {
+            case 1:
+                type = MoveType.Single;
+                break;
+            case 2:
+                type = MoveType.Double;
+                break;
+            default:
+                console.assert(false);
+        }
     }
 
     return {
@@ -19,11 +28,15 @@ function createMove(to, from) {
         toString: function() {
             switch (this.type) {
                 case MoveType.Single:
-                    return Util.squareToCoordinate(to)
+                    if (from >= 0 && from < 49) {
+                        return Util.squareToCoordinate(from) + Util.squareToCoordinate(to);
+                    } else {
+                        return Util.squareToCoordinate(to);
+                    }
                 case MoveType.Double:
-                    return Util.squareToCoordinate(from) + Util.squareToCoordinate(to)
+                    return Util.squareToCoordinate(from) + Util.squareToCoordinate(to);
                 case MoveType.Null:
-                    return '0000'
+                    return '0000';
             }
         }
     };
@@ -32,9 +45,9 @@ function createMove(to, from) {
 function createMoveFromString(moveString) {
     switch (moveString.length) {
         case 2:
-            return createMove(Util.coordinateToSquare(moveString))
+            return createMove(Util.coordinateToSquare(moveString));
         case 4:
-            return createMove(Util.coordinateToSquare(moveString.substr(2,2)), Util.coordinateToSquare(moveString.substr(0,2)))
+            return createMove(Util.coordinateToSquare(moveString.substr(2,2)), Util.coordinateToSquare(moveString.substr(0,2)));
     }
 }
 
